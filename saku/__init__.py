13fefba573f5feb82ba5de60475b60aa43622079
hppRC <hpp.ricecake@gmail.com>
2021-12-07 15:14:48 +0900

    :+1: improve regex

--- saku/__init__.py
+++ saku/__init__.py
@@ -1,4 +1,1 @@
-# import the contents of the Rust library into the Python extension
-# optional: include the documentation from the Rust module
-from saku import *
-from saku import __all__, __doc__
+from .saku import *
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use regex::{Captures, Regex};
 pub struct SentenceTokenizer {
     period: String,
     alt_period: String,
-    period_to: String,
+    period_newline: String,
     pattern: Regex,
 }
 
@@ -23,7 +23,7 @@ impl SentenceTokenizer {
         alt_period: Option<String>,
     ) -> PyResult<Self> {
         let period = period.unwrap_or(Self::DEFAULT_PERIOD.to_string());
-        let period_to = format!("{}\n", &period);
+        let period_newline = format!("{}\n", &period);
         let pattern = pattern.map(|p| Regex::new(&p).unwrap()).unwrap_or({
             patterns
                 .map(|patterns| Regex::new(&patterns.join("|")).unwrap())
@@ -32,39 +32,36 @@ impl SentenceTokenizer {
         let alt_period = alt_period.unwrap_or(Self::DEFAULT_ALT_PERIOD.to_string());
         Ok(Self {
             period,
-            period_to,
+            period_newline,
             pattern,
             alt_period,
         })
     }
 
-    fn tokenize_text(&self, text: &str) -> PyResult<Vec<String>> {
-        let text: String = self
+    fn tokenize(&self, text: &str, preserve_newline: Option<bool>) -> PyResult<Vec<String>> {
+        let preserve_newline = preserve_newline.unwrap_or(false);
+        let text = if preserve_newline {
+            text.to_string()
+        } else {
+            text.replace("\n", "").replace("\r", "")
+        };
+        Ok(self
             .pattern
-            .replace_all(text, |caps: &Captures| {
+            .replace_all(&text, |caps: &Captures| {
                 caps[0].replace(&self.period, &self.alt_period)
             })
-            .into();
-        let text = text.replace(&self.period, &self.period_to);
-        let sentences: Vec<String> = text
+            .to_string()
+            .replace(&self.period, &self.period_newline)
             .trim()
             .split("\n")
             .map(|s| s.replace(&self.alt_period, &self.period))
-            .collect();
-        Ok(sentences)
-    }
-
-    fn tokenize(&self, document: &str) -> PyResult<Vec<String>> {
-        Ok(document
-            .split("\n")
-            .map(|text| self.tokenize_text(text).unwrap())
-            .flatten()
             .collect())
     }
 }
 
 #[pymodule]
 fn saku(_: Python, m: &PyModule) -> PyResult<()> {
+    println!("bbb");
     m.add_class::<SentenceTokenizer>()?;
     Ok(())
 }
