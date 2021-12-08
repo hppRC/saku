0f5b86329a859affb8e25ac83a1e8b8aa959205f
hppRC <hpp.ricecake@gmail.com>
2021-12-08 20:56:12 +0900

    :+1: Done performance investigation

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,8 +1,8 @@
-use pyo3::prelude::*;
-// use pyo3::types::{PyString, PyList};
+use criterion::{criterion_group, criterion_main, Criterion};
 use regex::{Captures, Regex};
+use std::fs;
+use anyhow::Result;
 
-#[pyclass]
 pub struct SentenceTokenizer {
     period: String,
     alt_period: String,
@@ -10,19 +10,17 @@ pub struct SentenceTokenizer {
     pattern: Regex,
 }
 
-#[pymethods]
 impl SentenceTokenizer {
     const DEFAULT_PERIOD: &'static str = "。";
     const DEFAULT_ALT_PERIOD: &'static str = "__PERIOD__";
     const DEFAULT_PATTERN_STR: &'static str = r"（.*?）|「.*?」|『.*?』";
 
-    #[new]
-    fn __new__(
+    fn new(
         period: Option<String>,
         pattern: Option<String>,
         patterns: Option<Vec<String>>,
         alt_period: Option<String>,
-    ) -> PyResult<Self> {
+    ) -> Result<Self> {
         let period = period.unwrap_or(Self::DEFAULT_PERIOD.to_string());
         let period_newline = format!("{}\n", &period);
         let pattern = pattern.map(|p| Regex::new(&p).unwrap()).unwrap_or({
@@ -44,40 +42,42 @@ impl SentenceTokenizer {
         &self,
         mut text: String,
         preserve_newline: Option<bool>,
-    ) -> PyResult<Vec<String>> {
+    ) -> Result<Vec<String>> {
         let preserve_newline = preserve_newline.unwrap_or(false);
-        if !preserve_newline {
-            text = Regex::new(&format!(r"\n|\r"))
-                .unwrap()
-                .replace_all(&text, "")
-                .into_owned();
-        }
-        Ok(self
+        // if !preserve_newline {
+        //     text = Regex::new(&format!(r"\n|\r"))
+        //         .unwrap()
+        //         .replace_all(&text, "")
+        //         .into_owned();
+        // } // 35ms
+        let a = self
             .pattern
             .replace_all(&text, |caps: &Captures| {
                 caps[0].replace(&self.period, &self.alt_period)
-            })
-            .replace(&self.period, &self.period_newline)
-            .split("\n")
+            }) // 300ms
+            .replace(&self.period, &self.period_newline) // 100ms
+            .split("\n") // .collect::<Vec<_>>() 70ms
             .filter_map(|s| 
                 if s.is_empty() {
                     None
                 } else {
                     Some(s.replace(&self.alt_period, &self.period))
                 }
-            )
-            .collect())
+            ) // .collect::<Vec<_>>() 100ms
+            ;
+        //     .collect())
+        Ok(vec!["".into()])
     }
 }
 
-#[pymodule]
-fn saku(_: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<SentenceTokenizer>()?;
-    Ok(())
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("tokenize", |b| {
+        let data_path = "./data/medium.txt";
+        let tokenizer = SentenceTokenizer::new(None, None, None, None).unwrap();
+        let text = fs::read_to_string(data_path).unwrap();
+        b.iter(|| tokenizer.tokenize(text.clone(), None))
+    });
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {}
-}
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
