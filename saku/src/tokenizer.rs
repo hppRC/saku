9be5a9f553a70aa48ff87445ecfa4634b9549df5
hppRC <hpp.ricecake@gmail.com>
2021-12-15 21:12:41 +0900

    :+1: use control flow

--- saku/src/tokenizer.rs
+++ saku/src/tokenizer.rs
@@ -1,14 +1,21 @@
-use rustc_hash::FxHashSet;
+use rustc_hash::FxHashMap;
 use std::borrow::Cow;
 
 use crate::SentenceTokenizerBuilder;
 
 #[derive(Clone, Debug)]
+pub(crate) enum ControlFlow {
+    Eos,
+    LineBreaks,
+    RightParens(usize),
+    LeftParens(usize),
+}
+
+#[derive(Clone, Debug)]
 pub struct SentenceTokenizer {
     pub(crate) eos: char,
-    pub(crate) left_patterns: Vec<char>,
-    pub(crate) right_patterns: Vec<char>,
-    pub(crate) ch_set: FxHashSet<char>,
+    pub(crate) num_parens: usize,
+    pub(crate) chmap: FxHashMap<char, ControlFlow>,
 }
 
 impl Default for SentenceTokenizer {
@@ -19,19 +26,8 @@ impl Default for SentenceTokenizer {
 
 impl SentenceTokenizer {
     #[inline]
-    fn switch_flags_retun_in_parens(&self, ch: &char, flags: &mut Vec<bool>) -> bool {
-        for (l, f) in self.left_patterns.iter().zip(flags.iter_mut()) {
-            if ch == l {
-                *f = true;
-                return true;
-            }
-        }
-        let mut ret = false;
-        for (r, f) in self.right_patterns.iter().zip(flags.iter_mut()) {
-            *f &= !(ch == r);
-            ret |= *f;
-        }
-        ret
+    fn char_to_control_flow(&self, ch: &char) -> Option<&ControlFlow> {
+        self.chmap.get(ch)
     }
 
     #[inline]
@@ -39,42 +35,67 @@ impl SentenceTokenizer {
         self.tokenize_ignore_line_breaks(document)
     }
 
+    #[inline(always)]
+    pub fn process_line_breaks<'a>(&self, sentence: &mut String, start: &mut usize, i: usize, document: &'a str) {
+        sentence.push_str(&document[*start..i]);
+        *start = i + 1;
+    }
+
+    #[inline(always)]
+    pub fn process_left_parens<'a>(&self, flags: &mut Vec<usize>, nest_count: &mut usize, flag_id: usize) {
+        flags[flag_id] += 1;
+        *nest_count += 1;
+    }
+
+    #[inline(always)]
+    pub fn process_right_parens<'a>(&self, flags: &mut Vec<usize>, nest_count: &mut usize, flag_id: usize) {
+        if flags[flag_id] > 0 {
+            flags[flag_id] -= 1;
+            *nest_count -= 1;
+        }
+    }
+
+    #[inline(always)]
+    pub fn process_eos<'a>(&self, sentences: &mut Vec<Cow<'a, str>>, mut sentence: String, start: &mut usize, i: usize, nest_count: usize, eos_size: usize, document: &'a str) {
+        if nest_count > 0 {
+            return;
+        }
+        if sentence.is_empty() {
+            sentences.push(Cow::Borrowed(&document[*start..i + eos_size]));
+        } else {
+            sentence.push_str(&document[*start..i + eos_size]);
+            sentences.push(Cow::Owned(sentence));
+        }
+        *start = i + eos_size;
+    }
+
+
     #[inline]
     pub fn tokenize_ignore_line_breaks<'a>(&self, document: &'a str) -> Vec<Cow<'a, str>> {
         let document: &'a str = document.trim();
         let mut start: usize = 0;
         let mut sentences: Vec<Cow<'a, str>> = Vec::new();
         let mut sentence: String = String::new();
-        let mut flags: Vec<bool> = vec![false; self.left_patterns.len()];
+        let mut flags: Vec<usize> = vec![0; self.num_parens];
+        let mut nest_count: usize = 0;
         let eos_size = self.eos.len_utf8();
 
-        for (i, ch) in document
-            .char_indices()
-            .filter(|(_, ch)| self.ch_set.contains(ch))
-        {
-            if (ch == '\n') || (ch == '\r') {
-                sentence.push_str(&document[start..i]);
-                start = i + 1;
-                continue;
+        for (i, flow) in document.char_indices().filter_map(|(i, ch)| {
+            if let Some(flow) = self.char_to_control_flow(&ch) {
+                Some((i, flow))
+            } else {
+                None
             }
-
-            // CAUTION: This function call have a side effect in order to improve performance
-            // by switching flags and returning whether we are in parens or not simultaneously.
-            // Each flag of `flags` representing can be changed.
-            let in_parens = self.switch_flags_retun_in_parens(&ch, &mut flags);
-            if in_parens {
-                continue;
-            }
-
-            if ch == self.eos {
-                if sentence.is_empty() {
-                    sentences.push(Cow::Borrowed(&document[start..i + eos_size]));
-                } else {
-                    sentence.push_str(&document[start..i + eos_size]);
-                    sentences.push(Cow::Owned(sentence));
+        }) {
+            match *flow {
+                ControlFlow::LineBreaks => self.process_line_breaks(&mut sentence, &mut start, i, &document),
+                ControlFlow::LeftParens(flag_id) => self.process_left_parens(&mut flags, &mut nest_count, flag_id),
+                ControlFlow::RightParens(flag_id) => self.process_right_parens(&mut flags, &mut nest_count, flag_id),
+                ControlFlow::Eos => {
+                    self.process_eos(&mut sentences, sentence, &mut start, i, nest_count, eos_size, document);
                     sentence = String::new();
                 }
-                start = i + eos_size;
+                ,
             }
         }
         if start < document.len() {
@@ -93,41 +114,42 @@ impl SentenceTokenizer {
     // `tokenize` and `tokenize_raw` have a different return type to improve performance.
     #[inline]
     pub fn tokenize_raw<'a>(&self, document: &'a str) -> Vec<&'a str> {
+        todo!();
         let document = document.trim();
-        let mut start: usize = 0;
-        let mut sentences: Vec<&'a str> = Vec::new();
-        let mut flags: Vec<bool> = vec![false; self.left_patterns.len()];
-        let eos_size = self.eos.len_utf8();
+        // let mut start: usize = 0;
+        // let mut sentences: Vec<&'a str> = Vec::new();
+        // let mut flags: Vec<bool> = vec![false; self.left_patterns.len()];
+        // let eos_size = self.eos.len_utf8();
 
-        for (i, ch) in document
-            .char_indices()
-            .filter(|(_, ch)| self.ch_set.contains(ch))
-        {
-            if (ch == '\n') || (ch == '\r') {
-                if i != start {
-                    sentences.push(&document[start..i]);
-                }
-                start = i + 1;
-                continue;
-            }
+        // for (i, ch) in document
+        //     .char_indices()
+        //     .filter(|(_, ch)| self.ch_set.contains(ch))
+        // {
+        //     if (ch == '\n') || (ch == '\r') {
+        //         if i != start {
+        //             sentences.push(&document[start..i]);
+        //         }
+        //         start = i + 1;
+        //         continue;
+        //     }
 
-            // CAUTION: This function call have a side effect in order to improve performance
-            // by switching flags and returning whether we are in parens or not simultaneously.
-            // Each flag of `flags` representing can be changed.
-            let in_parens = self.switch_flags_retun_in_parens(&ch, &mut flags);
-            if in_parens {
-                continue;
-            }
+        //     // CAUTION: This function call have a side effect in order to improve performance
+        //     // by switching flags and returning whether we are in parens or not simultaneously.
+        //     // Each flag of `flags` representing can be changed.
+        //     let in_parens = self.switch_flags_retun_in_parens(&ch, &mut flags);
+        //     if in_parens {
+        //         continue;
+        //     }
 
-            if ch == self.eos {
-                sentences.push(&document[start..i + eos_size]);
-                start = i + eos_size;
-            }
-        }
-        if start < document.len() {
-            sentences.push(&document[start..document.len()]);
-        }
+        //     if ch == self.eos {
+        //         sentences.push(&document[start..i + eos_size]);
+        //         start = i + eos_size;
+        //     }
+        // }
+        // if start < document.len() {
+        //     sentences.push(&document[start..document.len()]);
+        // }
 
-        sentences
+        // sentences
     }
 }
